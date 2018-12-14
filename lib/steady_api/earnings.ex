89e35ea617eb7ead870c1044b921ddbc34cd7ba6
Angelika Tyborska <angelikatyborska@fastmail.com>
2018-12-14 09:28:33 +0100

    Simplify the API for estimating earnings

--- lib/steady_api/earnings.ex
+++ lib/steady_api/earnings.ex
@@ -7,20 +7,29 @@ defmodule SteadyAPI.Earnings do
 
   @type t :: %SteadyAPI.Earnings{}
 
-  @spec new(integer(), map()) :: SteadyAPI.Earnings.t()
-  def new(audience_count, params \\ %{}) do
+  @doc """
+    Takes statistics about the audience and estimates the earnings.
+
+    It truncates fractions of cents.
+  """
+  @spec estimate(integer(), map()) :: SteadyAPI.Earnings.t()
+  def estimate(audience_count, params \\ %{}) do
+    earnings = new(audience_count, params)
+
+    predicted =
+      earnings.audience_count
+      |> Kernel.*(earnings.conversion_percentage)
+      |> Kernel.*(0.01)
+      |> Kernel.*(earnings.monthly_amount_in_cents_per_user)
+      |> trunc()
+
+    %SteadyAPI.Earnings{earnings | predicted_monthly_earning_in_cents: predicted}
+  end
+
+  defp new(audience_count, params) do
     params = Map.take(params, [:conversion_percentage, :monthly_amount_in_cents_per_user])
 
     %SteadyAPI.Earnings{audience_count: audience_count}
     |> Map.merge(params)
-  end
-
-  @spec estimate(SteadyAPI.Earnings.t()) :: SteadyAPI.Earnings.t()
-  def estimate(earnings) do
-    predicted =
-      earnings.audience_count * earnings.conversion_percentage * 0.01 *
-        earnings.monthly_amount_in_cents_per_user
-
-    %SteadyAPI.Earnings{earnings | predicted_monthly_earning_in_cents: predicted}
   end
 end
--- test/steady_api/earnings_test.exs
+++ test/steady_api/earnings_test.exs
@@ -3,47 +3,30 @@ defmodule SteadyAPI.EarningsTest do
 
   alias SteadyAPI.Earnings
 
-  describe "new" do
-    test "has one required argument" do
-      actual = Earnings.new(123)
-      expected = %Earnings{audience_count: 123}
-
-      assert actual == expected
-    end
-
-    test "accepts additional params" do
-      actual = Earnings.new(123, %{conversion_percentage: 234})
-      expected = %Earnings{audience_count: 123, conversion_percentage: 234}
-
-      assert actual == expected
-    end
-
-    test "rejects random params" do
-      result = Earnings.new(123, %{conversion_percentage: 234, fruit: "banana"})
-
-      assert result.conversion_percentage == 234
-      assert Map.get(result, :fruit) == nil
-    end
-  end
-
   describe "estimate" do
     test "uses defaults" do
-      actual = Earnings.new(10_000) |> Earnings.estimate()
+      actual = Earnings.estimate(10_000)
       expected = %Earnings{audience_count: 10_000, predicted_monthly_earning_in_cents: 250_000}
 
       assert actual == expected
     end
 
-    test "returns fractions" do
+    test "rejects random params" do
+      result = Earnings.estimate(123, %{conversion_percentage: 234, fruit: "banana"})
+
+      assert result.conversion_percentage == 234
+      assert Map.get(result, :fruit) == nil
+    end
+
+    test "truncates fractions" do
       actual =
-        Earnings.new(152_541, %{monthly_amount_in_cents_per_user: 515, conversion_percentage: 7})
-        |> Earnings.estimate()
+        Earnings.estimate(10, %{monthly_amount_in_cents_per_user: 9, conversion_percentage: 1})
 
       expected = %Earnings{
-        audience_count: 152_541,
-        monthly_amount_in_cents_per_user: 515,
-        conversion_percentage: 7,
-        predicted_monthly_earning_in_cents: 5_499_103.050000001
+        audience_count: 10,
+        monthly_amount_in_cents_per_user: 9,
+        conversion_percentage: 1,
+        predicted_monthly_earning_in_cents: 0
       }
 
       assert actual == expected
@@ -52,19 +35,16 @@ defmodule SteadyAPI.EarningsTest do
     test "depends linearly on the audience count" do
       x1 =
         10_000
-        |> Earnings.new()
         |> Earnings.estimate()
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x10 =
         100_000
-        |> Earnings.new()
         |> Earnings.estimate()
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x100 =
         1_000_000
-        |> Earnings.new()
         |> Earnings.estimate()
         |> Map.get(:predicted_monthly_earning_in_cents)
 
@@ -75,20 +55,17 @@ defmodule SteadyAPI.EarningsTest do
     test "depends linearly on the conversion" do
       x1 =
         10_000
-        |> Earnings.new(%{conversion_percentage: 1})
-        |> Earnings.estimate()
+        |> Earnings.estimate(%{conversion_percentage: 1})
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x10 =
         10_000
-        |> Earnings.new(%{conversion_percentage: 10})
-        |> Earnings.estimate()
+        |> Earnings.estimate(%{conversion_percentage: 10})
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x100 =
         10_000
-        |> Earnings.new(%{conversion_percentage: 100})
-        |> Earnings.estimate()
+        |> Earnings.estimate(%{conversion_percentage: 100})
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       assert x1 * 10 == x10
@@ -98,20 +75,17 @@ defmodule SteadyAPI.EarningsTest do
     test "depends linearly on the monthly amount per user" do
       x1 =
         10_000
-        |> Earnings.new(%{monthly_amount_in_cents_per_user: 1})
-        |> Earnings.estimate()
+        |> Earnings.estimate(%{monthly_amount_in_cents_per_user: 1})
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x10 =
         10_000
-        |> Earnings.new(%{monthly_amount_in_cents_per_user: 10})
-        |> Earnings.estimate()
+        |> Earnings.estimate(%{monthly_amount_in_cents_per_user: 10})
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x100 =
         10_000
-        |> Earnings.new(%{monthly_amount_in_cents_per_user: 100})
-        |> Earnings.estimate()
+        |> Earnings.estimate(%{monthly_amount_in_cents_per_user: 100})
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       assert x1 * 10 == x10
