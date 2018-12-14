85791be7d95a9e95c8bf8e485a156167ebab5952
Angelika Tyborska <angelikatyborska@fastmail.com>
2018-12-14 08:35:09 +0100

    Add estimating earnings

--- lib/steady_api/earnings.ex
+++ lib/steady_api/earnings.ex
@@ -14,4 +14,13 @@ defmodule SteadyAPI.Earnings do
     %SteadyAPI.Earnings{audience_count: audience_count}
     |> Map.merge(params)
   end
+
+  @spec estimate(SteadyAPI.Earnings.t()) :: SteadyAPI.Earnings.t()
+  def estimate(earnings) do
+    predicted =
+      earnings.audience_count * earnings.conversion_percentage * 0.01 *
+        earnings.monthly_amount_in_cents_per_user
+
+    %SteadyAPI.Earnings{earnings | predicted_monthly_earning_in_cents: predicted}
+  end
 end
--- test/steady_api/earnings_test.exs
+++ test/steady_api/earnings_test.exs
@@ -25,4 +25,97 @@ defmodule SteadyAPI.EarningsTest do
       assert Map.get(result, :fruit) == nil
     end
   end
+
+  describe "estimate" do
+    test "uses defaults" do
+      actual = Earnings.new(10_000) |> Earnings.estimate()
+      expected = %Earnings{audience_count: 10_000, predicted_monthly_earning_in_cents: 250_000}
+
+      assert actual == expected
+    end
+
+    test "returns fractions" do
+      actual =
+        Earnings.new(152_541, %{monthly_amount_in_cents_per_user: 515, conversion_percentage: 7})
+        |> Earnings.estimate()
+
+      expected = %Earnings{
+        audience_count: 152_541,
+        monthly_amount_in_cents_per_user: 515,
+        conversion_percentage: 7,
+        predicted_monthly_earning_in_cents: 5_499_103.050000001
+      }
+
+      assert actual == expected
+    end
+
+    test "depends linearly on the audience count" do
+      x1 =
+        10_000
+        |> Earnings.new()
+        |> Earnings.estimate()
+        |> Map.get(:predicted_monthly_earning_in_cents)
+
+      x10 =
+        100_000
+        |> Earnings.new()
+        |> Earnings.estimate()
+        |> Map.get(:predicted_monthly_earning_in_cents)
+
+      x100 =
+        1_000_000
+        |> Earnings.new()
+        |> Earnings.estimate()
+        |> Map.get(:predicted_monthly_earning_in_cents)
+
+      assert x1 * 10 == x10
+      assert x1 * 100 == x100
+    end
+
+    test "depends linearly on the conversion" do
+      x1 =
+        10_000
+        |> Earnings.new(%{conversion_percentage: 1})
+        |> Earnings.estimate()
+        |> Map.get(:predicted_monthly_earning_in_cents)
+
+      x10 =
+        10_000
+        |> Earnings.new(%{conversion_percentage: 10})
+        |> Earnings.estimate()
+        |> Map.get(:predicted_monthly_earning_in_cents)
+
+      x100 =
+        10_000
+        |> Earnings.new(%{conversion_percentage: 100})
+        |> Earnings.estimate()
+        |> Map.get(:predicted_monthly_earning_in_cents)
+
+      assert x1 * 10 == x10
+      assert x1 * 100 == x100
+    end
+
+    test "depends linearly on the monthly amount per user" do
+      x1 =
+        10_000
+        |> Earnings.new(%{monthly_amount_in_cents_per_user: 1})
+        |> Earnings.estimate()
+        |> Map.get(:predicted_monthly_earning_in_cents)
+
+      x10 =
+        10_000
+        |> Earnings.new(%{monthly_amount_in_cents_per_user: 10})
+        |> Earnings.estimate()
+        |> Map.get(:predicted_monthly_earning_in_cents)
+
+      x100 =
+        10_000
+        |> Earnings.new(%{monthly_amount_in_cents_per_user: 100})
+        |> Earnings.estimate()
+        |> Map.get(:predicted_monthly_earning_in_cents)
+
+      assert x1 * 10 == x10
+      assert x1 * 100 == x100
+    end
+  end
 end
