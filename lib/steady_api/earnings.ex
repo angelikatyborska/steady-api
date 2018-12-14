3bc3996fb58343edad1abcde283c339277f5ff0d
Angelika Tyborska <angelikatyborska@fastmail.com>
2018-12-14 09:51:15 +0100

    Validate that values are non-negative integers

--- lib/steady_api/earnings.ex
+++ lib/steady_api/earnings.ex
@@ -9,13 +9,19 @@ defmodule SteadyAPI.Earnings do
 
   @doc """
     Takes statistics about the audience and estimates the earnings.
+    All statistics need to be integers, even the conversion percentage.
 
     It truncates fractions of cents.
   """
   @spec estimate(integer(), map()) :: SteadyAPI.Earnings.t()
   def estimate(audience_count, params \\ %{}) do
-    earnings = new(audience_count, params)
+    case new(audience_count, params) do
+      {:ok, earnings} -> {:ok, do_estimate(earnings)}
+      {:error, error} -> {:error, error}
+    end
+  end
 
+  defp do_estimate(earnings) do
     predicted =
       earnings.audience_count
       |> Kernel.*(earnings.conversion_percentage)
@@ -27,9 +33,33 @@ defmodule SteadyAPI.Earnings do
   end
 
   defp new(audience_count, params) do
-    params = Map.take(params, [:conversion_percentage, :monthly_amount_in_cents_per_user])
+    base = %SteadyAPI.Earnings{audience_count: audience_count}
+    params = Map.take(params, Map.keys(base) -- [:__struct__])
 
-    %SteadyAPI.Earnings{audience_count: audience_count}
+    base
     |> Map.merge(params)
+    |> validate()
+  end
+
+  defp validate(earnings) do
+    integer_values = [:monthly_amount_in_cents_per_user, :conversion_percentage, :audience_count]
+
+    errors =
+      integer_values
+      |> Enum.reduce([], fn key, errors ->
+        value = Map.get(earnings, key)
+
+        if is_integer(value) and value >= 0 do
+          errors
+        else
+          ["value of '#{key}' must be a non-negative integer (got #{inspect(value)})" | errors]
+        end
+      end)
+
+    if errors == [] do
+      {:ok, earnings}
+    else
+      {:error, Enum.join(errors, ", ")}
+    end
   end
 end
--- test/steady_api/earnings_test.exs
+++ test/steady_api/earnings_test.exs
@@ -6,28 +6,47 @@ defmodule SteadyAPI.EarningsTest do
   describe "estimate" do
     test "uses defaults" do
       actual = Earnings.estimate(10_000)
-      expected = %Earnings{audience_count: 10_000, predicted_monthly_earning_in_cents: 250_000}
+
+      expected =
+        {:ok, %Earnings{audience_count: 10_000, predicted_monthly_earning_in_cents: 250_000}}
 
       assert actual == expected
     end
 
-    test "rejects random params" do
-      result = Earnings.estimate(123, %{conversion_percentage: 234, fruit: "banana"})
+    test "ignores random params" do
+      {:ok, result} = Earnings.estimate(123, %{conversion_percentage: 234, fruit: "banana"})
 
       assert result.conversion_percentage == 234
       assert Map.get(result, :fruit) == nil
+    end
+
+    test "requires integers" do
+      actual =
+        Earnings.estimate(10.4, %{
+          conversion_percentage: 0,
+          monthly_amount_in_cents_per_user: -300
+        })
+
+      expected =
+        {:error,
+         "value of 'audience_count' must be a non-negative integer (got 10.4), " <>
+           "value of 'monthly_amount_in_cents_per_user' must be a non-negative integer (got -300)"}
+
+      assert actual == expected
     end
 
     test "truncates fractions" do
       actual =
         Earnings.estimate(10, %{monthly_amount_in_cents_per_user: 9, conversion_percentage: 1})
 
-      expected = %Earnings{
-        audience_count: 10,
-        monthly_amount_in_cents_per_user: 9,
-        conversion_percentage: 1,
-        predicted_monthly_earning_in_cents: 0
-      }
+      expected =
+        {:ok,
+         %Earnings{
+           audience_count: 10,
+           monthly_amount_in_cents_per_user: 9,
+           conversion_percentage: 1,
+           predicted_monthly_earning_in_cents: 0
+         }}
 
       assert actual == expected
     end
@@ -36,16 +55,19 @@ defmodule SteadyAPI.EarningsTest do
       x1 =
         10_000
         |> Earnings.estimate()
+        |> elem(1)
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x10 =
         100_000
         |> Earnings.estimate()
+        |> elem(1)
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x100 =
         1_000_000
         |> Earnings.estimate()
+        |> elem(1)
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       assert x1 * 10 == x10
@@ -56,16 +78,19 @@ defmodule SteadyAPI.EarningsTest do
       x1 =
         10_000
         |> Earnings.estimate(%{conversion_percentage: 1})
+        |> elem(1)
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x10 =
         10_000
         |> Earnings.estimate(%{conversion_percentage: 10})
+        |> elem(1)
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x100 =
         10_000
         |> Earnings.estimate(%{conversion_percentage: 100})
+        |> elem(1)
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       assert x1 * 10 == x10
@@ -76,16 +101,19 @@ defmodule SteadyAPI.EarningsTest do
       x1 =
         10_000
         |> Earnings.estimate(%{monthly_amount_in_cents_per_user: 1})
+        |> elem(1)
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x10 =
         10_000
         |> Earnings.estimate(%{monthly_amount_in_cents_per_user: 10})
+        |> elem(1)
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       x100 =
         10_000
         |> Earnings.estimate(%{monthly_amount_in_cents_per_user: 100})
+        |> elem(1)
         |> Map.get(:predicted_monthly_earning_in_cents)
 
       assert x1 * 10 == x10
